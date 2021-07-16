77a923ab089c01b2fdae0892dc1b03a569531bc8
Kashish Arora <kashisharora@google.com>
2021-07-16 21:36:14 +0000

    Create grpc client arguments struct

--- fbench/src/fbench/client.h
+++ fbench/src/fbench/client.h
@@ -8,37 +8,119 @@
 
 #define FBENCH_DELIMITER "\n[--xxyyzz--FBENCH_MAGIC_DELIMITER--zzyyxx--]\n"
 
+
 /**
- * This struct contains arguments used to control a single client.
+ * This struct contains basic arguments used to control a single client.
+ * Each client runs in a separate thread. This struct does not own the
+ * strings it references.
+**/
+struct BaseClientArguments
+{
+    public:
+        /**
+         * Sequential number identifying this client.
+         **/
+        int         _myNum;
+
+        /**
+         * The total number of clients controlled by the parent fbench
+         * application
+         **/
+        int         _totNum;
+
+        /**
+         * Pattern that combined with the client number will become the name
+         * of the file containing the urls this client should request.
+         **/
+        const char *_filenamePattern;
+
+        /**
+         * Pattern that combined with the client number will become the name
+         * of the file this client should dump url content to. If this
+         * pattern is set to NULL no output file is generated.
+         **/
+        const char *_outputPattern;
+
+        /**
+         * The minimum number of milliseconds between two requests from this
+         * client.
+         **/
+        long        _cycle;
+
+        /**
+         * Number of milliseconds to wait before making the first request.
+         * This will be different for different clients and helps distribute
+         * the requests.
+         **/
+        long        _delay;
+
+        /**
+         * Number of requests that should be made before we start logging
+         * response times. This is included so fbench startup slugginess
+         * will not affect the benchmark results.
+         **/
+        int         _ignoreCount;
+
+        /**
+         * Minimum number of bytes allowed in a response for a request to be
+         * successful. If a response contains fewer bytes than this number,
+         * the request will be logged as a failure even if no errors
+         * occurred.
+         **/
+        int         _byteLimit;
+
+        /**
+         * Number of times this client is allowed to re-use the urls in the
+         * input query file.
+         **/
+        int         _restartLimit;
+
+        /**
+        * Max line size in the input query data. Longer lines than this
+        * will be skipped.
+        **/
+        int         _maxLineSize;
+
+        /** Whether we should use POST in requests */
+        bool        _usePostMode;
+
+        /** Whether we should use gRPC to make requests */
+        bool        _useGrpcMode;
+
+
+        BaseClientArguments(int myNum, int totNum,
+                        const char *filenamePattern,
+                        const char *outputPattern,
+                        long cycle, long delay,
+                        int ignoreCount, int byteLimit,
+                        int restartLimit, int maxLineSize, bool postMode, bool grpcMode)
+            : _myNum(myNum),
+            _totNum(totNum),
+            _filenamePattern(filenamePattern),
+            _outputPattern(outputPattern),
+            _cycle(cycle),
+            _delay(delay),
+            _ignoreCount(ignoreCount),
+            _byteLimit(byteLimit),
+            _restartLimit(restartLimit),
+            _maxLineSize(maxLineSize),
+            _usePostMode(postMode),
+            _useGrpcMode(grpcMode)
+        {
+        }
+
+private:
+    BaseClientArguments(const BaseClientArguments &);
+    BaseClientArguments &operator=(const BaseClientArguments &);
+};
+
+/**
+ * This struct contains arguments used to control a single HTTP client.
  * Each client runs in a separate thread. This struct do not own the
  * strings it references.
  **/
-struct ClientArguments
+struct ClientArguments : public BaseClientArguments
 {
-    /**
-     * Sequential number identifying this client.
-     **/
-    int         _myNum;
-
-    /**
-     * The total number of clients controlled by the parent fbench
-     * application
-     **/
-    int         _totNum;
-
-    /**
-     * Pattern that combined with the client number will become the name
-     * of the file containing the urls this client should request.
-     **/
-    const char *_filenamePattern;
-
-    /**
-     * Pattern that combined with the client number will become the name
-     * of the file this client should dump url content to. If this
-     * pattern is set to NULL no output file is generated.
-     **/
-    const char *_outputPattern;
-
     /**
      * The server the client should fetch urls from.
      **/
@@ -47,47 +129,8 @@ struct ClientArguments
     /**
      * The server port where the webserver is running.
      **/
-    int         _port;
+    int         _port;      
 
-    /**
-     * The minimum number of milliseconds between two requests from this
-     * client.
-     **/
-    long        _cycle;
-
-    /**
-     * Number of milliseconds to wait before making the first request.
-     * This will be different for different clients and helps distribute
-     * the requests.
-     **/
-    long        _delay;
-
-    /**
-     * Number of requests that should be made before we start logging
-     * response times. This is included so fbench startup slugginess
-     * will not affect the benchmark results.
-     **/
-    int         _ignoreCount;
-
-    /**
-     * Minimum number of bytes allowed in a response for a request to be
-     * successful. If a response contains fewer bytes than this number,
-     * the request will be logged as a failure even if no errors
-     * occurred.
-     **/
-    int         _byteLimit;
-
-    /**
-     * Number of times this client is allowed to re-use the urls in the
-     * input query file.
-     **/
-    int         _restartLimit;
-
-    /**
-     * Max line size in the input query data. Longer lines than this
-     * will be skipped.
-     **/
-    int         _maxLineSize;
 
     /**
      * Indicate wether keep-alive connections should be enabled for this
@@ -100,9 +143,6 @@ struct ClientArguments
      * sending it
      **/
     bool        _base64Decode;
-
-    /** Whether we should use POST in requests */
-    bool        _usePostMode;
 
     /**
      * Indicate whether to add benchmark data coverage headers
@@ -127,22 +167,14 @@ struct ClientArguments
                     bool headerBenchmarkdataCoverage,
                     uint64_t queryfileOffset, uint64_t queryfileEndOffset, bool singleQueryFile,
                     const std::string & queryStringToAppend, const std::string & extraHeaders,
-                    const std::string &authority, bool postMode)
-        : _myNum(myNum),
-          _totNum(totNum),
-          _filenamePattern(filenamePattern),
-          _outputPattern(outputPattern),
-          _hostname(hostname),
+                    const std::string &authority, bool postMode, bool grpcMode)
+        :  BaseClientArguments(myNum, totNum, filenamePattern,
+        outputPattern, cycle, delay, ignoreCount, byteLimit, 
+        restartLimit, maxLineSize, postMode, grpcMode),
+        _hostname(hostname),
           _port(port),
-          _cycle(cycle),
-          _delay(delay),
-          _ignoreCount(ignoreCount),
-          _byteLimit(byteLimit),
-          _restartLimit(restartLimit),
-          _maxLineSize(maxLineSize),
           _keepAlive(keepAlive),
           _base64Decode(base64Decode),
-          _usePostMode(postMode),
           _headerBenchmarkdataCoverage(headerBenchmarkdataCoverage),
           _queryfileOffset(queryfileOffset),
           _queryfileEndOffset(queryfileEndOffset),
@@ -158,11 +190,50 @@ private:
     ClientArguments &operator=(const ClientArguments &);
 };
 
+/**
+ * This struct contains arguments used to control a single gRPC client.
+ * Each client runs in a separate thread. This struct do not own the
+ * strings it references.
+ **/
+struct GrpcClientArguments : public BaseClientArguments
+{
+    /**
+     * The IP address of the server that contains the index we are searching in.
+     **/
+    const char *_deployedIndexServerIp;
+
+    /**
+     * The id of the index we are searching in.
+     **/
+    const char *_deployedIndexId;
+
+    GrpcClientArguments(int myNum, int totNum,
+                    const char *filenamePattern,
+                    const char *outputPattern,
+                    long cycle, long delay,
+                    int ignoreCount, int byteLimit,
+                    int restartLimit, const char *deployedIndexServerIp, 
+                    const char *deployedIndexId, int maxLineSize, bool postMode, bool grpcMode)
+        :  BaseClientArguments(myNum, totNum, filenamePattern,
+        outputPattern, cycle, delay, ignoreCount, byteLimit, 
+        restartLimit, maxLineSize, postMode, grpcMode),
+        _deployedIndexServerIp(deployedIndexServerIp),
+        _deployedIndexId(deployedIndexId)
+    {
+    }
+
+private:
+    GrpcClientArguments(const GrpcClientArguments &);
+    GrpcClientArguments &operator=(const GrpcClientArguments &);
+};
 
 class Timer;
 class HTTPClient;
+class GrpcClient;
 class FileReader;
 struct ClientStatus;
+
+
 /**
  * This class implements a single test client. The clients are run in
  * separate threads to simulate several simultanious users. The
@@ -172,12 +243,14 @@ struct ClientStatus;
 class Client
 {
 private:
-    std::unique_ptr<ClientArguments> _args;
+    std::unique_ptr<ClientArguments> _httpArgs;
+    std::unique_ptr<GrpcClientArguments> _grpcArgs;
     std::unique_ptr<ClientStatus>    _status;
     std::unique_ptr<Timer>           _reqTimer;
     std::unique_ptr<Timer>           _cycleTimer;
     std::unique_ptr<Timer>           _masterTimer;
     std::unique_ptr<HTTPClient>      _http;
+    std::unique_ptr<GrpcClient>      _grpc;
     std::unique_ptr<FileReader>      _reader;
     std::unique_ptr<std::ofstream>   _output;
     int                              _linebufsize;
@@ -190,6 +263,8 @@ private:
     Client &operator=(const Client &);
     static void runMe(Client * client);
     void run();
+    void runGrpc();
+    void runHttp();
 
 public:
     typedef std::unique_ptr<Client> UP;
@@ -199,6 +274,12 @@ public:
      **/
     Client(vespalib::CryptoEngine::SP engine, ClientArguments *args);
 
+     /**
+     * The gRPC client arguments given to this method becomes the
+     * responsibility of the client.
+     **/
+    Client(GrpcClientArguments *args);
+    
     /**
      * Delete objects owned by this client, including the client arguments.
      **/
